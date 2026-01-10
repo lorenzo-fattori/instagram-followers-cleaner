49cdc3448719ed071211ec2af97d603d48f0d0db
Lorenzo Fattori <lorenzo.fattori@icloud.com>
2026-01-10 15:35:33 +0100

    Create example_usage.py

---
+++ example_usage.py
@@ -0,0 +1,20 @@
+from instagram_cleaner import clean_input, not_following_back
+
+followers_raw = '''
+user1
+user2
+'''
+following_raw = '''
+user1
+user2
+user3
+'''
+
+followers = clean_input(followers_raw)
+following = clean_input(following_raw)
+
+not_mutual = not_following_back(followers, following)
+
+print(f"Users not following back: {len(not_mutual)}")
+for user in not_mutual:
+    print(user)
