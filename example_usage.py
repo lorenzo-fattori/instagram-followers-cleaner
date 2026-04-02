df2372f9a67d904edd74828c49838f86c8c672c9
lorenzofattori <lorenzo.fattori@icloud.com>
2026-04-02 11:29:42 +0200

    Update example_usage.py with new follower data

--- example_usage.py
+++ example_usage.py
@@ -1,14 +1,14 @@
 from instagram_cleaner import clean_input, not_following_back
 
 followers_raw = '''
-user1
-user2
+some_follower1
+some_follower2
+some_follower3
 '''
 following_raw = '''
-user1
-user2
-user3
-user4
+some_follower1
+some_follower4
+some_follower5
 '''
 
 followers = clean_input(followers_raw)
@@ -19,3 +19,8 @@ not_mutual = not_following_back(followers, following)
 print(f"Users not following back: {len(not_mutual)}")
 for user in not_mutual:
     print(user)
+    
+# Output:
+# Users not following back: 2
+# some_follower4
+# some_follower5
